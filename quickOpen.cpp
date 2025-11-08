568d1bf6e12e321fcaf024c6129065f72783786e
ProudPurple <andersonkimball5@gmail.com>
2025-11-08 17:26:14 -0500

    Added Oppening Apps directly ontop of vscode

--- quickOpen.cpp
+++ quickOpen.cpp
@@ -8,37 +8,57 @@ int main() {
     std::string projectDir = "";
 
     std::cout << "What u want twin: ";
-    std::string whatuwant;
+    std::string whatuwant, exeFile;
     std::cin >> whatuwant;
 
     if (whatuwant == "fbla") {
         vcvars64 = R"(C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat)";
         projectDir = R"(C:\Users\ander\OneDrive\Desktop\Work\Coding\FBLA-Project\Virtual-Pet)";
+        exeFile = "virtual-pet.exe";
     } else if (whatuwant == "quickopen") {
+        exeFile = "quickOpen.exe";
         vcvars64 = R"(C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat)";
         projectDir = R"(C:\Users\ander\OneDrive\Desktop\Work\Coding\Actually Useful\Console Shorties)";
-    }
+    } else return 0;
 
-    // cmd.exe /k keeps console open
-std::string fullCmd = "cmd.exe /c call \"" + vcvars64 + "\" && start /B \"\" code \"" + projectDir + "\" && exit";
+    std::cout << "What part: ";
+    std::string whatpart;
+    std::cin >> whatpart;
 
+    std::string fullCmd ;
 
+    if (whatpart == "app") {
+        std::string exePath = projectDir + "\\" + exeFile;
+        // create mutable buffer for CreateProcess
+        std::vector<char> cmdLine(exePath.begin(), exePath.end());
+        cmdLine.push_back('\0');
 
+        STARTUPINFOA si{};
+        PROCESS_INFORMATION pi{};
+        si.cb = sizeof(si);
 
-    std::vector<char> cmdLine(fullCmd.begin(), fullCmd.end());
-    cmdLine.push_back('\0');
+        // Provide the working directory as the 8th parameter
+        if (!CreateProcessA(nullptr, cmdLine.data(), nullptr, nullptr, FALSE, 0, nullptr,
+                        projectDir.c_str(), &si, &pi)) {
+            std::cerr << "CreateProcess failed, error: " << GetLastError() << "\n";
+            return 1;
+        }
+    } else if (whatpart == "code") {
+        std::vector<char> cmdLine(fullCmd.begin(), fullCmd.end());
+        cmdLine.push_back('\0');
 
-    STARTUPINFOA si{};
-    PROCESS_INFORMATION pi{};
-    si.cb = sizeof(si);
+        STARTUPINFOA si{};
+        PROCESS_INFORMATION pi{};
+        si.cb = sizeof(si);
 
-    if (!CreateProcessA(nullptr, cmdLine.data(), nullptr, nullptr, FALSE, CREATE_NO_WINDOW, nullptr, nullptr, &si, &pi)) {
-        std::cerr << "CreateProcess failed, error: " << GetLastError() << "\n";
-        return 1;
-    }
+        if (!CreateProcessA(nullptr, cmdLine.data(), nullptr, nullptr, FALSE, CREATE_NO_WINDOW, nullptr, nullptr, &si, &pi)) {
+            std::cerr << "CreateProcess failed, error: " << GetLastError() << "\n";
+            return 1;
+        }
 
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    } else return 0;
     return 0;
 }
 
